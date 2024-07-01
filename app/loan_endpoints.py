764594366c9bcccce426314b2914f9966fef3bc2
vpat510 <vinit9patel@gmail.com>
2024-07-01 17:56:04 -0500

    implement loan crud

--- app/loan_endpoints.py
+++ app/loan_endpoints.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from . import models, schemas, database
 
+# Create router for loan endpoints
 router = APIRouter()
 
-# Dependency
+# Dependency -> get database session
 def get_db():
     db = database.SessionLocal()
     try:
@@ -12,13 +13,46 @@ def get_db():
     finally:
         db.close()
 
+# fetch a loan by it's ID and return it. Raise 404 if not found
+def get_loan_by_id(loan_id: int, db: Session):
+    loan = db.query(models.Loan).filter(models.Loan.id == loan_id).first()
+    if loan is None:
+        raise HTTPException(status_code=404, detail="Loan not found")
+    return loan
 
-# Create Loan
 
-# Read Loan
+# POST / Create new loan, add to database, commit transaction & return created loan
+@router.post("/loans/", response_model=schemas.Loan)
+def create_loan(loan: schemas.LoanCreate, db: Session = Depends(get_db)):
+    #mode_dump -> convert loan into pydantic model to a dictionary & then create a new `Loan` ORM instance using that dict
+    new_loan = models.Loan(**loan.model_dump())
+    db.add(new_loan)
+    db.commit()
+    db.refresh(new_loan)
+    return new_loan
 
-# Update Loan
 
-# Delete Loan
+# GET / Read Loan details by loan ID
+@router.get("/loans/{loan_id}", response_model=schemas.Loan)
+def read_loan(loan_id: int, db: Session = Depends(get_db)):
+    return get_loan_by_id(loan_id, db)
 
+
+# PUT / Update Loan details by Loan & return updated loan
+@router.put("/loans/{loan_id}", response_model=schemas.Loan)
+def update_loan(loan_id: int, loan: schemas.LoanCreate, db: Session = Depends(get_db)):
+    db_loan = get_loan_by_id(loan_id, db)
+    for key, value in loan.model_dump().items():
+        setattr(db_loan, key, value)
+    db.commit()
+    db.refresh(db_loan)
+    return db_loan
+
+# DELETE / Delete Loan by loan ID, commit transaction & return deleted loan
+@router.delete("/loans/{loan_id}", response_model=schemas.Loan)
+def delete_loan(loan_id: int, db: Session = Depends(get_db)):
+    db_loan = get_loan_by_id(loan_id, db)
+    db.delete(db_loan)
+    db.commit()
+    return db_loan
 
--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from . import models
 from .user_endpoints import router as user_router
+from .loan_endpoints import router as loan_router
+
 
 # Create all database tables
 Base.metadata.create_all(bind=engine)
@@ -10,6 +12,8 @@ app = FastAPI()
 
 # Include the User router
 app.include_router(user_router, prefix="/api/v1")
+app.include_router(loan_router, prefix="/api/v1")
+
 
 @app.get("/")
 def read_root():
